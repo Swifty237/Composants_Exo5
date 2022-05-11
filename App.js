356cf48bd1454f4c43c714602fb509b224beb715
yannick <yannickkamdemkouam@yahoo.fr>
2022-05-11 16:22:50 +0200

    init button

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.buttonBox}>
+        <Button
+          title="Press me"
+          color="green"
+          onPress={() => alert('Bravo...')}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +17,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
+
+  buttonBox: {
+    width: 250
+  }
 });
